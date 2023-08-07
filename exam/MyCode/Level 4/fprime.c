9ad71f5451a3226fc092e3eafbcf314d619e260d
rong5026 <rong5026@naver.com>
2023-08-07 20:23:01 +0900

    [ADD] fprime

---
+++ exam/MyCode/Level 4/fprime.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int ac, char **av)
+{
+    if(ac == 2)
+    {
+        int number = atoi(av[1]);
+
+        if(number == 1)
+            printf("2");
+        int i = 2;
+        while(number >= i)
+        {
+            if(number & i == 0)
+            {
+                printf("%d", i);
+                if(number / i == 1)
+                    break;
+                printf("*");
+                number /= i;
+                i = 1;
+            }
+
+            i++;
+        }
+    }
+    printf("\n");
+    return (0);
+}

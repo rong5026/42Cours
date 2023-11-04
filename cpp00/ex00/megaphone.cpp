34641916b319d971187e5c1cd26e38fb04e78b54
rong5026 <rong5026@naver.com>
2023-11-04 16:12:00 +0900

    [ADD] cpp00 ex00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hong-yeonghwan <hong-yeonghwan@student.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/04 15:59:56 by hong-yeongh       #+#    #+#             */
+/*   Updated: 2023/11/04 16:11:45 by hong-yeongh      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cstdlib>
+
+using std::cout;
+using std::endl;
+
+std::string set_upper(std::string arg)
+{
+    for (size_t i = 0; i < arg.length(); i += 1)
+        arg[i] = toupper(arg[i]);
+    return arg;
+}
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+        cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << endl;
+	else
+	{
+		for (int i = 1; i < argc; i += 1)
+        	cout << set_upper(argv[i]);
+    	cout << endl;
+	}
+    return (EXIT_SUCCESS);
+}
